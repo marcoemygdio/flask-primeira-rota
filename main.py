ed5a68f4064f2de544d931e5f8d3f25ec047ddc0
Marco Antonio <101368238+marcoemygdio@users.noreply.github.com>
2022-03-17 22:46:57 +0000

    correcao

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ app = Flask('app')
 @app.route('/')
 def hello_world():
   return '<h1>Minha primeira rota!</h1>'
-
-app.run(host='0.0.0.0', port=8080)
+  
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
